5746ce51fbe9aeaa5a3dd6bbfb008ec88fe1b7ae
AdrianMunene <adrianmunene62@gmail.com>
2025-04-10 22:35:25 +0300

    frontend works, particulate matter chart displays

--- webserver/backend/src/main.rs
+++ webserver/backend/src/main.rs
@@ -1,5 +1,6 @@
-use axum::{ routing::{get, post}, Router, Extension };
+use axum::{ http::Method, routing::{ get, post }, Router, Extension };
 use dotenvy::dotenv;
+use tower_http::cors::{ CorsLayer, Any };
 
 use database::establish_connection_pool;
 use handlers::{create_air_quality_record, get_air_quality_record};
@@ -13,9 +14,12 @@ async fn main() {
 
     let pool = establish_connection_pool();
 
+    let cors = CorsLayer::new().allow_methods(vec![Method::GET, Method::POST]).allow_origin(Any);
+
     let app = Router::new()
     .route("/airquality", get(get_air_quality_record))
     .route("/airquality", post(create_air_quality_record))
+    .layer(cors)
     .layer(Extension(pool));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
