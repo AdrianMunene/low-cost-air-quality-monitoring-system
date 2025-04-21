605472390d0d5575854f707e5604212a5ef37a01
AdrianMunene <adrianmunene62@gmail.com>
2025-04-21 21:09:25 +0300

    Rolled back src/database.rs to the state from April 1st

--- webserver/backend/src/database.rs
+++ webserver/backend/src/database.rs
@@ -2,7 +2,6 @@ use diesel::sqlite::SqliteConnection;
 use diesel::r2d2::{ self, ConnectionManager };
 use dotenvy::dotenv;
 use std::env;
-use std::time::Duration;
 
 pub type DatabasePool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
@@ -13,29 +12,5 @@ pub fn establish_connection_pool() -> DatabasePool {
 
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
 
-    let pool = r2d2::Pool::builder()
-        .connection_timeout(Duration::from_secs(5))
-        .connection_customizer(Box::new(ConnectionOptions))
-        .test_on_check_out(true)
-        .build(manager)
-        .expect("Failed to create pool");
-
-    // Test the connection pool immediately
-    pool.get().expect("Failed to verify database connection on startup");
-
-    println!("Database connection pool successfully established");
-    
-    pool
-}
-
-#[derive(Debug)]
-struct ConnectionOptions;
-
-impl r2d2::CustomizeConnection<SqliteConnection, r2d2::Error> for ConnectionOptions {
-    fn on_acquire(&self, conn: &mut SqliteConnection) -> Result<(), r2d2::Error> {
-        use diesel::Connection;
-        conn.begin_test_transaction()
-            .map_err(|e| r2d2::Error::QueryError(e))?;
-        Ok(())
-    }
+    r2d2::Pool::builder().build(manager).expect("Failed to create pool")
 }
